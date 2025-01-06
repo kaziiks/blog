0b26183b7fbec7d08a342defb7aec5582a561398
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-06 09:48:51 +0200

    skibidi

--- index.php
+++ index.php
@@ -7,6 +7,16 @@ $config = require ("config.php");
 $db = new database($config["Database"]);
 $posts = $db->query("SELECT * FROM posts")->fetchAll();
 
+if (isset($_GET["search_query"]) &&$_GET["search_query"] != ""){
+    echo"fent reactor";
+    dd("SELECT * FROM posts WHERE content LIKE" . $_GET["search_query"]);
+    $posts = $db->query("SELECT * FROM posts WHERE content LIKE" . $_GET["search_query"])->fetchall();
+}
+
+echo "<form>";
+echo "<input name='search_query' />";
+echo "<button>meklet</button>";
+echo "</form>";
 
 echo"<ul>";
 foreach($posts as $post){
