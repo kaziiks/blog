720ecd57a158c34eb6b7854d8c3842c61f5a707f
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-02-04 13:39:51 +0200

    fentdits

--- routes.php
+++ routes.php
@@ -5,5 +5,5 @@ return[
     "/show" => "controllers/posts/show.php",
     "/about" => "controllers/story.php",
     "/create" => "controllers/posts/create.php",
-    "/edit" => "controllers/posts/create.php"
+    "/edit" => "controllers/posts/edit.php"
 ];
